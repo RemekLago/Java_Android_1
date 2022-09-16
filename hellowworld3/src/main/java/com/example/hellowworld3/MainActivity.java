8246479d0e55a51fca2afff9a461b2aac1de3529
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-09-16 18:26:23 +0200

    update

---
+++ hellowworld3/src/main/java/com/example/hellowworld3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hellowworld3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edit1 = findViewById(R.id.edit1);
+        EditText edit2 = findViewById(R.id.edit2);
+        EditText edit3 = findViewById(R.id.edit3);
+
+        edit1.setTextColor(Color.RED);
+        edit2.setTextSize(32);
+        edit3.setBackgroundColor(Color.BLUE);
+
+        // Getting Input from EditText
+        String name = edit1.getText().toString();
+        String phone = edit2.getText().toString();
+        String password = edit3.getText().toString();
+    }
+}
