2c0d83ecee16e2092595371a4b2e3381ef5ca0df
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-09-16 15:59:26 +0200

    update

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.text1);
+        TextView textView1 = findViewById(R.id.text1);
+        textView1.setText("Hello");
+        textView1.setTextColor(Color.RED);
+        textView1.setTextSize(32);
+        textView1.setBackgroundColor(Color.GREEN);
+
+        TextView textView2 = findViewById(R.id.text2);
+
     }
 }
