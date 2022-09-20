165012fbdbb816ed69807ec3492a702895df8af0
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-09-20 15:25:33 +0200

    update

--- grid_layout/two_activities/src/main/java/com/example/two_activities/MainActivity.java
+++ grid_layout/two_activities/src/main/java/com/example/two_activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.two_activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(view.getContext(), MainActivity2.class);
                 i.putExtra("myName", "Hello My Friend");
+                startActivity(i);
+            }
+        });
+
+        Button button2 = findViewById(R.id.buttonGoogle);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
                 startActivity(i);
             }
         });
