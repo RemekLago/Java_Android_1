cfea0481df02993ab835bb51dc84c76d23e5c480
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-09-22 19:14:37 +0200

    update

--- adapter_listview2/src/main/java/com/example/adapter_listview2/MainActivity.java
+++ adapter_listview2/src/main/java/com/example/adapter_listview2/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.adapter_listview2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +43,34 @@ public class MainActivity extends AppCompatActivity {
         MyListAdapter adapter = new MyListAdapter(this, mainTitle, subTitle, imagesArray);
         listView.setAdapter(adapter);
 
-
+        // second solution
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    Toast.makeText(getApplicationContext(), "First Item has been clicked", Toast.LENGTH_SHORT).show();
+                } else if (i == 1) {
+                    Toast.makeText(getApplicationContext(), "Second Item has been clicked", Toast.LENGTH_SHORT).show();
+                } else if (i == 2) {
+                    Toast.makeText(getApplicationContext(), "Third Item has been clicked", Toast.LENGTH_SHORT).show();
+                }else if (i == 3) {
+                    Toast.makeText(getApplicationContext(), "Forth Item has been clicked", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+        }
+        });
     }
 }
+
+                // 4. Handling Click Event
+//        listView.setOnClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                if (i == 0){
+//                    Toast.makeText(getApplicationContext(), "First Item has been clicked", Toast.LENGTH_SHORT).show();
+//                }else if (i == 1){
+//                    Toast.makeText(getApplicationContext(), "Second Item has been clicked", Toast.LENGTH_SHORT).show();
+//                }else if (i == 2){
+//                Toast.makeText(getApplicationContext(), "Third Item has been clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
