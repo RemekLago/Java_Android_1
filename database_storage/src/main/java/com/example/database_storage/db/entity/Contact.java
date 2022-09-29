1ebd56a6f1341b3592ddfcf1ee564b9005d564f9
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-09-29 10:00:18 +0200

    update

--- database_storage/src/main/java/com/example/database_storage/db/entity/Contact.java
+++ database_storage/src/main/java/com/example/database_storage/db/entity/Contact.java
@@ -1,4 +1,47 @@
 package com.example.database_storage.db.entity;
 
 public class Contact {
+
+    public static final String TABLE_NAME = "contacts";
+    public static final String COLUMN_ID = "contact_id";
+    public static final String COLUMN_EMAIL = "contact_email";
+
+
+    private String name;
+    private String email;
+    private String id;
+
+    private Contact() {
+
+    }
+
+    public Contact(String name, String email, String id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
