7bb80bd116133e4e056c9a3e30c7247c8a8aba49
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-10-06 09:19:04 +0200

    update

---
+++ scrollingcable/src/main/java/com/scrollingcable/DataBaseCable.java
@@ -0,0 +1,16 @@
+package com.scrollingcable;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseCable extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
