d31c5ae05729c987604643264d56d96689750375
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-10-06 13:11:35 +0200

    update

--- scrollingcable/src/main/java/com/scrollingcable/MainActivity.java
+++ scrollingcable/src/main/java/com/scrollingcable/MainActivity.java
@@ -1,14 +1,28 @@
 package com.scrollingcable;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import com.scrollingcable.db.DataBaseCable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private DataBaseCable dataBaseCable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataBaseCable = new DataBaseCable(this);
+
+        try{
+            addKonfekcja("Konfekcja1", "Magazyn1", 500 );
+            Cursor cursor = takeKonfekcja();
+            showKonfekcja(cursor);
+        } finally{
+            dataBaseCable.close();
+        }
+
     }
 }
--- scrollingcable/src/main/java/com/scrollingcable/db/DataBaseCable.java
+++ scrollingcable/src/main/java/com/scrollingcable/db/DataBaseCable.java
@@ -1,10 +1,12 @@
-package com.scrollingcable;
+package com.scrollingcable.db;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import com.scrollingcable.MainActivity;
 
 public class DataBaseCable extends SQLiteOpenHelper {
     private static final String DATA_BASE_NAME = "DataBaseCable.db";
@@ -15,18 +17,18 @@ public class DataBaseCable extends SQLiteOpenHelper {
         super(context, DATA_BASE_NAME, factory, DATA_BASE_VERSION);
     }
 
+    public DataBaseCable(Context context) {
+        super(context, DATA_BASE_NAME, null, DATA_BASE_VERSION);
+    }
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + DataBaseColumns.TABLE_NAME + " (" +
                 DataBaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                DataBaseColumns.STARA_KONFEKCJA + " TEXT, " +
-                DataBaseColumns.STARA_KONFEKCJA_LOKALIZACJA + " TEXT NOT NULL, " +
-                DataBaseColumns.ILOSC_METROW_PRZED + " INTEGER, " +
-                DataBaseColumns.ILOSC_METROW_PO + " INTEGER, " +
-                DataBaseColumns.ILOSC_METROW_PRZEWINIETO + " INTEGER, " +
-                DataBaseColumns.NOWA_KONFEKCJA + " TEXT, " +
-                DataBaseColumns.NOWA_KONFEKCJA_DLUGOSC + " INTEGER, " +
-                DataBaseColumns.NOWA_KONFEKCJA_LOKALIZACJA + " TEXT" +" )"
+                DataBaseColumns.KONFEKCJA + " TEXT, " +
+                DataBaseColumns.KONFEKCJA_LOKALIZACJA + " TEXT NOT NULL, " +
+                DataBaseColumns.KONFEKCJA_ILOSC_METROW + " INTEGER, " + " )"
                 );
     }
 
