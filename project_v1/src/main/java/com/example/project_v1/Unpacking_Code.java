3cc5ee66cff872f0c23b83e9b87e35375abc4a16
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-10-12 09:04:18 +0200

    update

--- project_v1/src/main/java/com/example/project_v1/Unpacking_Code.java
+++ project_v1/src/main/java/com/example/project_v1/Unpacking_Code.java
@@ -1,6 +1,17 @@
 package com.example.project_v1;
 
+import java.util.ArrayList;
+
 public class Unpacking_Code {
 
-    String code_for_scann_label = "WYGPZ2201681|         1897 013 05|0,300|0|S300-2205||562105_1|749955|NEW|||124/A/05/22||||20.05.22|||||||"
+    public static String code_for_scan_label = "WYGPZ2201681|         1897 013 05|0,300|0|S300-2205||562105_1|749955|NEW|||124/A/05/22||||20.05.22|||||||";
+    public static String[] c = code_for_scan_label.split("|");
+
+    public static void Read() {
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
+    }
+
+
 }
