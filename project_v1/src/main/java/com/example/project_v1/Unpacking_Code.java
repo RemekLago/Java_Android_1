deab24d5514c5b175cae76dec890ac7347babcc9
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-10-12 08:46:04 +0200

    update

---
+++ project_v1/src/main/java/com/example/project_v1/Unpacking_Code.java
@@ -0,0 +1,6 @@
+package com.example.project_v1;
+
+public class Unpacking_Code {
+
+    String code_for_scann_label = "WYGPZ2201681|         1897 013 05|0,300|0|S300-2205||562105_1|749955|NEW|||124/A/05/22||||20.05.22|||||||"
+}
--- scrollingcable/src/main/java/com/scrollingcable/MainActivity.java
+++ scrollingcable/src/main/java/com/scrollingcable/MainActivity.java
@@ -14,12 +14,16 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.scrollingcable.activities_on_database.ActivitiesOnDatabase;
 import com.scrollingcable.db.DataBaseColumns;
 import com.scrollingcable.db.DataCable;
 
 public class MainActivity extends AppCompatActivity {
 
+    String input_konfekcja = "";
+    String input_lokalizacja = "";
+    String input_ilosc = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +32,15 @@ public class MainActivity extends AppCompatActivity {
 
         dataBaseCable = new DataCable(this);
 
-        Button pokaz_Button = findViewById(R.id.pokaz_button);
+        Button zatwierdz_button = findViewById(R.id.zatwierdz_button);
+        Button pokaz_button = findViewById(R.id.pokaz_button);
+        TextInputEditText numer_konfekcji_input = findViewById(R.id.numer_konfekcji_input);
+        TextInputEditText ilosc_metrow_input = findViewById(R.id.ilosc_metrow_input);
+        TextInputEditText lokalizacja_input = findViewById(R.id.lokalizacja_input);
 
-        pokaz_Button.setOnClickListener(new View.OnClickListener() {
+
+
+        pokaz_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, CableList.class);
@@ -40,8 +50,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        zatwierdz_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input_konfekcja = numer_konfekcji_input.getText().toString();
+                input_lokalizacja = lokalizacja_input.getText().toString();
+                input_ilosc = ilosc_metrow_input.getText().toString();
+            }
+        });
+
         try{
-            ActivitiesOnDatabase.addKonfekcja("Konfekcja3", "Magazyn3", 700 );
+            ActivitiesOnDatabase.addKonfekcja(input_konfekcja, input_lokalizacja, Integer.parseInt(input_ilosc));
             Cursor cursor = ActivitiesOnDatabase.takeKonfekcja();
             ActivitiesOnDatabase.showKonfekcja(cursor);
         } finally{
