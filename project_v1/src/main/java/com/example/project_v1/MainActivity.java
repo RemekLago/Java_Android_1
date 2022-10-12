54e18f00069831dc6359907d071600df872df8de
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-10-12 16:19:18 +0200

    update

--- project_v1/src/main/java/com/example/project_v1/MainActivity.java
+++ project_v1/src/main/java/com/example/project_v1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +12,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Unpacking_Code.Read();
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+        TextView textView3 = findViewById(R.id.textView3);
+        TextView textView4 = findViewById(R.id.textView4);
+        TextView textView5 = findViewById(R.id.textView5);
+        TextView textView6 = findViewById(R.id.textView6);
+        TextView textView7 = findViewById(R.id.textView7);
+        TextView textView8 = findViewById(R.id.textView8);
+        TextView textView9 = findViewById(R.id.textView9);
+        TextView textView10 = findViewById(R.id.textView10);
+        TextView textView11 = findViewById(R.id.textView11);
+        TextView textView12 = findViewById(R.id.textView12);
+        TextView textView13 = findViewById(R.id.textView13);
+        TextView textView14 = findViewById(R.id.textView14);
+
+        textView1.setText(Unpacking_Code.text1);
+        textView2.setText(Unpacking_Code.text2);
+        textView3.setText(Unpacking_Code.text3);
+        textView4.setText(Unpacking_Code.text4);
+        textView5.setText(Unpacking_Code.text5);
+        textView6.setText(Unpacking_Code.text6);
+        textView7.setText(Unpacking_Code.text7);
+        textView8.setText(Unpacking_Code.text8);
+        textView9.setText(Unpacking_Code.text9);
+        textView10.setText(Unpacking_Code.text10);
+        textView11.setText(Unpacking_Code.text11);
+        textView12.setText(Unpacking_Code.text12);
+        textView13.setText(Unpacking_Code.text13);
+        textView14.setText(Unpacking_Code.text14);
+
+
     }
-
-
 }
--- project_v1/src/main/java/com/example/project_v1/Unpacking_Code.java
+++ project_v1/src/main/java/com/example/project_v1/Unpacking_Code.java
@@ -1,17 +1,24 @@
 package com.example.project_v1;
 
-import java.util.ArrayList;
-
 public class Unpacking_Code {
 
-    public static String code_for_scan_label = "WYGPZ2201681|         1897 013 05|0,300|0|S300-2205||562105_1|749955|NEW|||124/A/05/22||||20.05.22|||||||";
-    public static String[] c = code_for_scan_label.split("|");
+    public static String code_for_scan_label = "1233|3456|345";
+//            "WYGPZ2201681|         1897 013 05|0,300|0|S300-2205||562105_1|749955|NEW|||124/A/05/22||||20.05.22|||||||";
+    public static String[] code = code_for_scan_label.split("\\|");
 
-    public static void Read() {
-        for (int i = 0; i < c.length; i++) {
-            System.out.println(c[i]);
-        }
-    }
-
+    static String text1 = code[0];
+    static String text2 = code[1].trim();
+    static String text3 = code[2];
+    static String text4 = code[3];
+    static String text5 = code[4];
+    static String text6 = code[5];
+    static String text7 = code[6];
+    static String text8 = code[7]; //pusty
+    static String text9 = code[8]; //pusty
+    static String text10 = code[9];
+    static String text11 = code[10]; //pusty
+    static String text12 = code[11]; //pusty
+    static String text13 = code[12]; //pusty
+    static String text14 = code[13];
 
 }
